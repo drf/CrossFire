2a3532e580de41894ff284cf189f509b010ff190
Dario Freddi <drf54321@gmail.com>
2009-05-30 20:34:10 +0200

    A bomb
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

---
+++ src/items/Bomb.java
@@ -0,0 +1,59 @@
+package items;
+
+import gameChart.Box;
+import gameLogic.Attackable;
+import globals.Entity;
+
+public class Bomb extends globals.Entity implements Attackable {
+
+	int hp;
+	
+	public Bomb() {
+		hp = 10;
+	}
+	
+	@Override
+	public int getDexterity() {
+		return 0;
+	}
+
+	@Override
+	public int getHp() {
+		return hp;
+	}
+
+	@Override
+	public int getLuck() {
+		return 0;
+	}
+
+	@Override
+	public int getStrength() {
+		return 10;
+	}
+
+	@Override
+	public void onDeath() {
+		// Deal 10 damage to each unity in the adjacent boxes
+		for (Box box : getBox().getAdjacentBoxes()) {
+			Entity ent = box.getChart().getTokenOn(box);
+			if (ent instanceof Attackable) {
+				int newhp = ((Attackable)ent).getHp();
+				
+				if (newhp <= 0) {
+					newhp = 0;
+					((Attackable)ent).setHp(newhp);
+					((Attackable)ent).onDeath();
+				} else {
+					((Attackable)ent).setHp(newhp);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void setHp(int value) {
+		hp = value;
+	}
+
+}
