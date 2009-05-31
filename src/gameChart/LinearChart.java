d045fc3540b10c5e1cf985474ebaef8cc48c022e
Snagg <snagg@nemesis.local>
2009-05-31 18:15:26 +0200

    modified:   src/gameChart/LinearChart.java

--- src/gameChart/LinearChart.java
+++ src/gameChart/LinearChart.java
@@ -10,17 +10,7 @@ public class LinearChart extends BidimentionalChart {
 	}
 	
 	public LinearChart(int width) {
-		setSize(width, 0);
-	}
-
-	protected Pair<Integer> getBoxPosition(Box b) {		
-		for (int i = 0; i < getWidth(); ++i) {
-				if (getChart()[i][0] == b) {
-					return new Pair<Integer>(i, 0);
-				}
-		}
-		// Return a null pair
-		return new Pair<Integer>();
+		setSize(width, 1);
 	}
 		
 }
