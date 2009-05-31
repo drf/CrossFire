26dff6ac65ab8a3278b72cf0e814f45eb5936ffb
Dario Freddi <drf54321@gmail.com>
2009-05-31 19:03:00 +0200

    This somehow works, but it's still ugly and sucks
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

--- src/characters/CharacterTest.java
+++ src/characters/CharacterTest.java
@@ -16,6 +16,10 @@ public class CharacterTest {
 	 */
 	public static void main(String[] args) {
 		
+		System.out.println(ItemGenerator.generateCasualPickable().toString());
+		System.out.println(ItemGenerator.generateCasualPickable().toString());
+		System.out.println(ItemGenerator.generateCasualPickable().toString());
+		
 		Random r = new Random();
 		System.out.println("Creazione personaggi");
 		Set<Character> chars = new HashSet<Character>();
--- src/items/ItemGenerator.java
+++ src/items/ItemGenerator.java
@@ -1,5 +1,6 @@
 package items;
 
+import java.util.HashSet;
 import java.util.Random;
 
 import gameLogic.Equipable;
@@ -7,15 +8,74 @@ import gameLogic.Pickable;
 
 public class ItemGenerator {
 	
+	private static HashSet<Class<?>> items() {
+		HashSet<Class<?>> retset = new HashSet<Class<?>>();
+		
+		retset.add(Bow.class);
+		retset.add(LongSword.class);
+		retset.add(MagicStick.class);
+		retset.add(Potion.class);
+		retset.add(Shield.class);
+		
+		return retset;
+	}
+	
 	public static Equipable generateCasualEquipable() {
-		return null;
+		HashSet<Equipable> items = new HashSet<Equipable>();
+		
+		for (Class<?> type : items()) {
+			for (Class<?> item : type.getInterfaces()) {
+				if (item == Equipable.class) {
+					try {
+						items.add((Equipable)item.newInstance());
+					} catch (InstantiationException e) {
+						
+					} catch (IllegalAccessException e) {
+						
+					}
+				}
+			}
+		}
+		
+		Random r = new Random();
+		return (Equipable) items.toArray()[r.nextInt(items.size())];
 	}
 
 	public static Consumable generateCasualConsumable() {
-		return null;
+		HashSet<Consumable> items = new HashSet<Consumable>();
+		
+		for (Class<?> type : items()) {
+			for (Class<?> item : type.getInterfaces()) {
+				if (item == Consumable.class) {
+					try {
+						items.add((Consumable)item.newInstance());
+					} catch (InstantiationException e) {
+						
+					} catch (IllegalAccessException e) {
+						
+					}
+				}
+			}
+		}
+		
+		Random r = new Random();
+		return (Consumable) items.toArray()[r.nextInt(items.size())];
 	}
 	
 	public static Pickable generateCasualPickable() {
-		return null;
+		HashSet<Pickable> items = new HashSet<Pickable>();
+		
+		for (Class<?> type : items()) {
+			try {
+				items.add((Pickable)type.newInstance());
+			} catch (InstantiationException e) {
+
+			} catch (IllegalAccessException e) {
+
+			}
+		}
+		
+		Random r = new Random();
+		return (Pickable) items.toArray()[r.nextInt(items.size())];
 	} 
 }
