c159e5ad751d757aea4c922cb0016307e6c9a521
Dario Freddi <drf54321@gmail.com>
2009-06-02 15:49:54 +0200

    Starting javadoc for characters
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

--- src/characters/Character.java
+++ src/characters/Character.java
@@ -4,12 +4,34 @@ import gameChart.Box;
 import gameChart.City;
 import gameChart.Hill;
 import gameLogic.Equipable;
+import gameLogic.Movable;
 import gameLogic.PickException;
 import gameLogic.Pickable;
 import globals.Entity;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * <b>Character</b> represents a {@link Fighter} that is the main entity a human
+ * player can control. Characters react to terrain changes, can equip and use items,
+ * and are able to perform a set of attacks. 
+ * <p>
+ * Character is abstract: in fact a valid character has a race, one between:
+ * <ul>
+ * <li>{@link Human}</li>
+ * <li>{@link Elf}</li>
+ * <li>{@link Orc}</li>
+ * <li>{@link Wizard}</li>
+ * </ul>
+ *
+ * More information on these races can be found in each race's class
+ * 
+ * @author Dario Freddi
+ * @author Vincenzo Iozzo
+ * 
+ * @see Fighter
+ */
 public abstract class Character extends Fighter implements gameLogic.Attackable,
                                 gameLogic.CanMeleeAttack, gameLogic.CanPick, gameLogic.Movable {
 	/**
@@ -18,6 +40,12 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 	private static final long serialVersionUID = 2790814461885173594L;
 	private ArrayList<Equipable> itemsList = new ArrayList<Equipable>();
 	
+	/**
+	 * Represent a race of a Character
+	 * 
+	 * @author Dario Freddi
+	 * @author Vincenzo Iozzo
+	 */
 	public enum Race {
 		Human,
 		Elf,
@@ -25,14 +53,36 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		Wizard
 	}
 	
+	/**
+	 * Default constructor, create a character with random attributes
+	 * 
+	 * @see Fighter.Fighter()
+	 */
 	public Character() {
 		super();
 	} 
 	
+	/**
+	 * Creates a new character with the given attributes
+	 * 
+	 * @param intelligence the character's intelligence
+	 * @param strength the character's strength
+	 * @param dexterity the character's dexterity
+	 * @param magicskill the character's magic skill
+	 * @param luck the character's luck
+	 * @param HP the character's health points
+	 */
 	public Character(int intelligence, int strength, int dexterity, int magicskill, int luck, int HP) {
 		super(intelligence, strength, dexterity, magicskill, luck, 100);
 	}
 	
+	/**
+	 * Check if the character is able to pick the given {@link Pickable}.
+	 * 
+	 * @param i the {@link Pickable} to check
+	 * @return whether the character is able to pick it or not
+	 * @see Pickable
+	 */
 	public Boolean canPick(Pickable i) {
 		if (i.getMaximumRequirements() != null) {
 			if (i.getMaximumRequirements().compareTo(getAttributes()) < 0) {
@@ -49,6 +99,14 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		return true;
 	}
 	
+	/**
+	 * Pick a {@link Pickable} and resolve it. If it is {@link Equipable}, the item
+	 * will be equipped. The modifiers of the {@link Pickable} will be resolved when
+	 * this function gets called
+	 * 
+	 * @param i the {@link Pickable} to pick
+	 * @throws PickException
+	 */
 	public void pick(Pickable i) throws PickException {
 		if (this.canPick(i)) {
 			
@@ -63,6 +121,13 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		}
 	}
 	
+	/**
+	 * Removes from the equipment an {@link Equipable}, removing its modifiers
+	 * from the character.
+	 * 
+	 * @param i the {@link Equipable} to remove
+	 * @throws PickException
+	 */
 	public void unequip(Equipable i) throws PickException {
 		removeTerrainChanges(getBox());
 		setAttibutes(i.getModifier().resetAttrs(getAttributes()));
@@ -73,19 +138,36 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		}
 	}
 	
+	/**
+	 * Unequips and drops on the current box an {@link Equipable}
+	 * 
+	 * @param i the {@link Equipable} to drop
+	 * @throws PickException
+	 * @see unequip
+	 */
 	public void dropEquip(Equipable i) throws PickException {
 		unequip(i);
 		((Entity)i).setBox(getBox());	
 	}
 	
+	/**
+	 * @return whether this character can perform a ranged attack or not
+	 */
 	public boolean canRangedAttack() {
 		return (getDexterity() + getIntelligence() >= 80);
 	}
 	
+	/**
+	 * @return whether this character can perform a magic attack or not
+	 */
 	public boolean canMagicAttack() {
 		return (getIntelligence() + getMagicSkill() >= 150);
 	}
  	
+	/**
+	 * Reimplemented from {@link Attackable}. The character will drop all
+	 * of its equipment on the current box
+	 */
 	public void onDeath() {
 		for(Equipable i: this.itemsList) {
 			if (i instanceof Equipable) {
@@ -98,6 +180,9 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		}
 	}
 	
+	/**
+	 * @return the damage reduction for this character
+	 */
 	public int getDamageReduction() {
 		int damageReduction = 0;
 		
@@ -107,6 +192,9 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		return damageReduction;
 	}
 
+	/**
+	 * @return the melee attack bonus for this character
+	 */
 	public int getMeleeAttackBonus() {
 		int bonusMeleeDamage = 0;
 		
@@ -116,6 +204,9 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		return bonusMeleeDamage;
 	}
 	
+	/**
+	 * @return the magic damage bonus for this character
+	 */
 	public int getMagicDamageBonus() {
 		int bonusMagicDamage = 0;
 		
@@ -125,6 +216,9 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		return bonusMagicDamage;
 	}
 
+	/**
+	 * @return the ranged attack bonus for this character
+	 */
 	public int getRangedAttackBonus() {
 		int bonusRangedDamage = 0;
 		
@@ -134,11 +228,24 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		return bonusRangedDamage;
 	}
 	
+	/**
+	 * Reimplemented from {@link Movable}. Applies terrain changes according to
+	 * the box type
+	 * 
+	 * @param oldBox the old box
+	 * @param newBox the box on which the character has moved
+	 */
 	public void boxChanged(Box oldBox, Box newBox) {
 		removeTerrainChanges(oldBox);
 		applyTerrainChanges(newBox);
 	}
 	
+	/**
+	 * Applies some changes to the attributes according to the type of the terrain,
+	 * according to the specifics of the game
+	 * 
+	 * @param box the box on which the character stands
+	 */
 	private void applyTerrainChanges(Box box) {
 		if (box instanceof Hill) {
 			if (getDexterity() < 50) {
@@ -155,6 +262,12 @@ public abstract class Character extends Fighter implements gameLogic.Attackable,
 		}
 	}
 	
+	/**
+	 * Removes changes made to the attributes according to the type of the terrain,
+	 * according to the specifics of the game
+	 * 
+	 * @param box the box on which the character stands
+	 */
 	private void removeTerrainChanges(Box box) {
 		if (box instanceof Hill) {
 			if (getDexterity() < 50) {
--- src/characters/Fighter.java
+++ src/characters/Fighter.java
@@ -6,6 +6,17 @@ import java.util.Random;
 import globals.BaseAttributes;
 import globals.PlayableEntity;
 
+/**
+ * Fighter is the main class for each playable entity that has combat attributes.
+ * Each class inheriting from fighter will be provided with all the attributes belonging
+ * to {@link BaseAttributes} and a name.
+ * 
+ * 
+ * @author Dario Freddi
+ * @author Vincenzo Iozzo
+ * @see BaseAttributes
+ *
+ */
 public abstract class Fighter extends PlayableEntity {
 
 	/**
@@ -15,6 +26,10 @@ public abstract class Fighter extends PlayableEntity {
 	private String name;
 	private BaseAttributes attributes;
 
+	/**
+	 * Default constructor. Create a new fighter with random attributes, each one ranging
+	 * from 0 to 100
+	 */
 	public Fighter() {
 		super();
 		Random r = new Random();
@@ -22,10 +37,29 @@ public abstract class Fighter extends PlayableEntity {
 				r.nextInt(101), r.nextInt(101), 100);		
 	}
 	
+	/**
+	 * Create a new fighter with the specified parameters
+	 * 
+	 * @param intelligence the fighter's intelligence
+	 * @param strength the fighter's strength
+	 * @param dexterity the fighter's dexterity
+	 * @param magicskill the fighter's magic skill
+	 * @param luck the fighter's luck
+	 * @param HP the fighter's health points
+	 */
 	public Fighter(int intelligence, int strength, int dexterity, int magicskill, int luck, int HP) {
 		attributes = new BaseAttributes(intelligence, strength, dexterity, magicskill, luck, HP);
 	}
 	
+	/**
+	 * Generate a set of random numbers, ranging from min and max. This function
+	 * can be used to generate semi-random attributes starting from a base rule
+	 * 
+	 * @param min The minimum value of the randomizer
+	 * @param max The maximum value of the randomizer
+	 * @param n The quantity of numbers that should be generated
+	 * @return
+	 */
 	public static ArrayList<Integer> randomAttributes(int min, int max, int n) {
 		Random r = new Random();
 		int index, delta;
@@ -55,50 +89,86 @@ public abstract class Fighter extends PlayableEntity {
 		return ret;
 	}
 	
+	/**
+	 * @return The strength of the fighter
+	 */
 	public int getStrength() {
 		return attributes.getStrength();
 	}
 	
+	/**
+	 * @return The dexterity of the fighter
+	 */
 	public int getDexterity() {
 		return attributes.getDexterity();
 	}
 	
+	/**
+	 * @return The intelligence of the fighter
+	 */
 	public int getIntelligence() {
 		return attributes.getIntelligence();
 	}
 	
+	/**
+	 * @return The magic skill of the fighter
+	 */
 	public int getMagicSkill() {
 		return attributes.getMagicSkill();
 	}
 	
+	/**
+	 * @return The luck of the fighter
+	 */
 	public int getLuck() {
 		return attributes.getLuck();
 	}
 	
+	/**
+	 * @return The health points of the fighter
+	 */
 	public int getHp() {
 		return attributes.getHp();
 	}
 	
+	/**
+	 * @param value the new strength for the fighter
+	 */
 	public void setStrength(int value) {
 		attributes.setStrength(value);
 	}
 	
+	/**
+	 * @param value the new dexterity for the fighter
+	 */
 	public void setDexterity(int value) {
 		attributes.setDexterity(value);
 	}
 
+	/**
+	 * @param value the new intelligence for the fighter
+	 */
 	public void setIntelligence(int value) {
 		attributes.setIntelligence(value);
 	}
 
+	/**
+	 * @param value the new magic skill for the fighter
+	 */
 	public void setMagicSkill(int value) {
 		attributes.setMagicSkill(value);
 	}
 
+	/**
+	 * @param value the new luck for the fighter
+	 */
 	public void setLuck(int value) {
 		attributes.setLuck(value);
 	}
 	
+	/**
+	 * @param value the new health points for the fighter
+	 */
 	public void setHp(int value) {
 		attributes.setHp(value);
 	}
@@ -107,10 +177,20 @@ public abstract class Fighter extends PlayableEntity {
 		return "fighter:" + this.name;
 	}
 	
+	/**
+	 * Set the new attributes for this fighter
+	 * 
+	 * @param newAttrs a {@link BaseAttributes} object carrying the new attributes
+	 */
 	public void setAttibutes(BaseAttributes newAttrs) {
 		this.attributes = newAttrs;
 	}
 	
+	/**
+	 * Get all the attributes for this fighter
+	 * 
+	 * @return a {@link BaseAttributes} object carrying the attributes of the fighter
+	 */
 	public BaseAttributes getAttributes() {
 		return this.attributes;
 	}
