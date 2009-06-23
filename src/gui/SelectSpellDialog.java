12bc76ab584c56d972dd164aabb806d33e5a9a2d
Dario Freddi <drf54321@gmail.com>
2009-06-23 22:16:45 +0200

    Spelldialog
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

---
+++ src/gui/SelectSpellDialog.java
@@ -0,0 +1,145 @@
+package gui;
+import gameLogic.CanMagicAttack;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListModel;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import org.jdesktop.application.Application;
+
+import spells.Spell;
+
+import characters.Wizard;
+import com.cloudgarden.layout.AnchorConstraint;
+import com.cloudgarden.layout.AnchorLayout;
+
+import com.sun.org.apache.xml.internal.security.c14n.CanonicalizerSpi;
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
+public class SelectSpellDialog extends javax.swing.JDialog {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7318512858787587112L;
+	
+	private JList spellList;
+	private JLabel costLabel;
+	private JLabel jLabel1;
+	private JLabel descLabel;
+	private JLabel jl1;
+	private JButton okButton;
+	private CanMagicAttack caster;
+
+	/**
+	* Auto-generated main method to display this JDialog
+	*/
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame();
+				SelectSpellDialog inst = new SelectSpellDialog(frame, new Wizard());
+				inst.setVisible(true);
+			}
+		});
+	}
+	
+	public SelectSpellDialog(JFrame frame, CanMagicAttack caster) {
+		super(frame);
+		this.caster = caster;
+		initGUI();
+		DefaultListModel model = new DefaultListModel();
+		int index = 0;
+		for (Spell spell : caster.getAvailableSpells()) {
+			model.add(index, spell.getName());
+			++index;
+		}
+		spellList.setModel(model);
+	}
+	
+	private void initGUI() {
+		try {
+			{
+				AnchorLayout thisLayout = new AnchorLayout();
+				getContentPane().setLayout(thisLayout);
+			}
+			{
+				ListModel spellListModel = 
+					new DefaultComboBoxModel(
+							new String[] { "Item One", "Item Two" });
+				spellList = new JList();
+				getContentPane().add(spellList, new AnchorConstraint(45, 343, 798, 31, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL));
+				spellList.setModel(spellListModel);
+				spellList.setPreferredSize(new java.awt.Dimension(122, 208));
+				spellList.addListSelectionListener(new ListSelectionListener() {
+					public void valueChanged(ListSelectionEvent evt) {
+						spellListValueChanged(evt);
+					}
+				});
+			}
+			{
+				okButton = new JButton();
+				getContentPane().add(okButton, new AnchorConstraint(864, 615, 940, 392, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL));
+				okButton.setName("okButton");
+				okButton.setPreferredSize(new java.awt.Dimension(87, 21));
+			}
+			{
+				jl1 = new JLabel();
+				getContentPane().add(jl1, new AnchorConstraint(45, 615, 96, 423, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL));
+				jl1.setName("jl1");
+				jl1.setPreferredSize(new java.awt.Dimension(75, 14));
+			}
+			{
+				descLabel = new JLabel();
+				getContentPane().add(descLabel, new AnchorConstraint(45, 1065, 465, 645, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL));
+				descLabel.setName("descLabel");
+				descLabel.setPreferredSize(new java.awt.Dimension(164, 116));
+			}
+			{
+				jLabel1 = new JLabel();
+				getContentPane().add(jLabel1, new AnchorConstraint(545, 494, 596, 423, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL));
+				jLabel1.setName("jLabel1");
+				jLabel1.setPreferredSize(new java.awt.Dimension(28, 14));
+			}
+			{
+				costLabel = new JLabel();
+				getContentPane().add(costLabel, new AnchorConstraint(545, 1080, 596, 645, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL, AnchorConstraint.ANCHOR_REL));
+				costLabel.setName("costLabel");
+				costLabel.setPreferredSize(new java.awt.Dimension(170, 14));
+			}
+			Application.getInstance().getContext().getResourceMap(getClass()).injectComponents(getContentPane());
+			pack();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void spellListValueChanged(ListSelectionEvent evt) {
+		int index = 0;
+		for (Spell spell : caster.getAvailableSpells()) {
+			if (spellList.getSelectedIndex() == index) {
+				descLabel.setText(spell.getDescription());
+				costLabel.setText(Integer.toBinaryString(spell.getCost()));
+			}
+			++index;
+		}
+	}
+
+}
