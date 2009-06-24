f1cf4c69f43d984f58628af89cff7efef7214aed
Dario Freddi <drf54321@gmail.com>
2009-06-25 01:02:28 +0200

    You will die, you will die
    Signed-off-by: Dario Freddi <drf54321@gmail.com>

--- src/gameChart/CircolarChart.java
+++ src/gameChart/CircolarChart.java
@@ -11,12 +11,7 @@ public class CircolarChart extends BidimensionalChart {
 
 	public CircolarChart(int width, int height) {
 		setSize(width, height);
-	}
-	
-	@Override
-	public Set<Box> getAdjacentBoxes(Box b) {
-		// TODO Auto-generated method stub
-		return null;
+		createChart();
 	}
 
 }
